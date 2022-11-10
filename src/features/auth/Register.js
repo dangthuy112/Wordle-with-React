f0a3889c50c8ca28ba653bff097f7f7ede982ba4
dangthuy112 <47605433+dangthuy112@users.noreply.github.com>
2022-11-10 15:43:38 -0600

    Update Register.js

--- src/features/auth/Register.js
+++ src/features/auth/Register.js
@@ -1,49 +1,20 @@
-import { useEffect, useState } from 'react'
-import { Button, Modal, Box, Grid, Avatar, Typography, TextField, Paper, IconButton, Link, Alert } from '@mui/material'
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+@@ -0,0 +1,87 @@
+import { useState } from 'react'
+import { Button, Modal, Box, Grid, Avatar, Typography, TextField, Paper, IconButton, Link } from '@mui/material'
 import ArrowBackOutlinedIcon from '@mui/icons-material/ArrowBackOutlined';
-import { useDispatch } from 'react-redux';
-import { setCredentials } from './authSlice';
-import { useLoginMutation } from './authApiSlice';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
 
 export default function Register({ setCurrentModal }) {
     const [open, setOpen] = useState(true);
     const handleClose = () => { };
-    const dispatch = useDispatch();
-    const [user, setUser] = useState('');
-    const [pwd, setPwd] = useState('');
-    const [errMsg, setErrMsg] = useState(null);
-
-    const handleWelcome = () => {
-        setCurrentModal('welcome')
-    }
 
     const handleRegisterButton = () => {
-        setCurrentModal('register')
+        setCurrentModal('register');
     }
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        try {
-            setUser('');
-            setPwd('');
-            setOpen(false);
-        } catch (err) {
-            if (!err?.originalStatus) {
-                setErrMsg('No Server Response');
-            } else if (err.originalStatus === 400) {
-                setErrMsg('Missing Username or Password');
-            } else if (err.originalStatus === 401) {
-                setErrMsg('Username and Password doesn\'t match');
-            } else {
-                setErrMsg('Login Failed');
-            }
-        }
+    const handleWelcomeButton = () => {
+        setCurrentModal('welcome');
     }
-
-    const handleUserInput = (e) => setUser(e.target.value);
-    const handlePwdInput = (e) => setPwd(e.target.value);
 
     return (
         <div>
@@ -53,33 +24,42 @@ export default function Register({ setCurrentModal }) {
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
             >
-                <Paper style={paperStyle}>
-                    <Grid container alignItems="center" direction="column" justifyContent="space-around">
-                        <Grid container alignItems="left">
-                            <IconButton sx={{ color: 'white', backgroundColor: '#1565c0' }} onClick={handleWelcome} >
+                <Grid>
+                    <Paper style={paperStyle}>
+                        <Box
+                            display='flex'
+                            alignItems="left"
+                        >
+                            <IconButton sx={{ color: 'white', backgroundColor: '#1565c0' }}
+                                onClick={handleWelcomeButton}>
                                 <ArrowBackOutlinedIcon />
                             </IconButton>
-                        </Grid>
+                        </Box>
                         <Grid align='center'>
-                            <Avatar style={avatarStyle}><LockOutlinedIcon /></Avatar>
+                            <Avatar style={avatarStyle}><AccountBoxIcon /></Avatar>
                             <Typography variant="h4" sx={{
                                 color: 'white',
-                                margin: '5px 0 20px 0'
+                                margin: '5px 0 10px 0'
                             }}>Register Account</Typography>
                         </Grid>
-                        {errMsg && <Alert severity='error' variant="outlined" sx={{ color: '#f44336', margin: '-5px 0 10px 0' }}>{errMsg}</Alert>}
-                        <TextField label='Username' placeholder='Enter Username' variant='filled'
-                            fullWidth required onChange={handleUserInput}
-                            sx={{ input: { color: 'white' }, placeholder: { color: 'white' }, label: { color: 'white' } }} />
-                        <TextField label='Password' placeholder='Enter Password' variant='filled'
-                            fullWidth required type='password' onChange={handlePwdInput}
-                            sx={{ mt: '8px', input: { color: 'white' }, placeholder: { color: 'white' }, label: { color: 'white' } }} />
-                        <Button type='submit' color='primary' variant='contained' fullWidth style={btnStyle} onClick={handleSubmit}
-                        >Sign In</Button>
-                        <Link component="button" variant="body2" sx={{ color: 'white' }} onClick={handleRegisterButton}
-                        >Need an account? Sign Up</Link>
-                    </Grid>
-                </Paper>
+                        <TextField label='Username' placeholder='Enter Username' fullWidth required
+                            sx={{
+                                input: { color: 'white' }, placeholder:
+                                    { color: 'white' }, label: { color: 'white' }
+                            }} />
+                        <TextField label='Password' placeholder='Enter Password' fullWidth required type='password'
+                            sx={{
+                                mt: '5px', input: { color: 'white' }, placeholder:
+                                    { color: 'white' }, label: { color: 'white' }
+                            }} />
+                        <TextField label='Confirm Password' placeholder='Enter Same Password' fullWidth required type='password'
+                            sx={{
+                                mt: '5px', input: { color: 'white' }, placeholder:
+                                    { color: 'white' }, label: { color: 'white' }
+                            }} />
+                        <Button type='submit' color='primary' variant='contained' fullWidth style={btnStyle}>Sign In</Button>
+                    </Paper>
+                </Grid>
             </Modal>
         </div >
     )
@@ -89,10 +69,10 @@ const paperStyle = {
     padding: '10px 20px 20px 20px',
     margin: "20px auto",
     position: 'absolute',
-    top: '40%',
+    top: '45%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    height: '50vh',
+    height: '60vh',
     width: 280,
     backgroundColor: '#545454',
     border: '2px solid #CCC',
